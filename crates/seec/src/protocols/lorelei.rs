c745028b6ab7be13a4a424966ac5abe419e40b1c
Maximilian Stillger <maximilian.stillger@arcor.de>
2024-12-19 20:51:28 +0100

    more

--- crates/seec/src/protocols/lorelei.rs
+++ crates/seec/src/protocols/lorelei.rs
@@ -71,20 +71,6 @@ pub struct LoreleiShare<R> {
 }
 
 
-
-
-/// Contains preprocessing data (`[\delta_ab]_i`) for interactive gates in
-/// **reverse** topological order. This is needed to evaluate interactive gates.
-#[derive(Clone, Default)]
-pub struct SetupData {
-    eval_shares: Vec<EvalShares>,
-}
-
-#[derive(Clone)]
-pub struct EvalShares {
-    shares: BitVec,
-}
-
 // Note: arithmetic has only multiplication by constant
 #[derive(Copy, Clone, PartialOrd, Ord, PartialEq, Eq, Hash, Debug, Serialize, Deserialize)]
 pub enum ArithmeticGate<R> {
@@ -119,72 +105,9 @@ pub enum LoreleiGate<R> {
 
 
 
-pub type AbySetupMsg = executor::Message<BooleanGmw>;
-
-pub struct AbySetupProvider<Mtp> {
-    party_id: usize,
-    mt_provider: Mtp,
-    sender: seec_channel::Sender<AbySetupMsg>,
-    receiver: seec_channel::Receiver<AbySetupMsg>,
-    setup_data: Option<SetupData>,
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct AstraSetupMsg(BitVec);
-
-#[derive(Debug, Copy, Clone)]
-pub enum InputBy {
-    P0,
-    P1,
-}
-pub struct AstraSetupHelper {
-    sender: MultiSender<AstraSetupMsg>,
-    // shared rng with p0
-    priv_seed_p0: [u8; 32],
-    // shared rng with p1
-    priv_seed_p1: [u8; 32],
-    // shared between p0 and p1
-    joint_seed: [u8; 32],
-}
-pub struct AstraSetupProvider {
-    // The normal parties have party id 0 and 1. For the helper, there is a dedicated struct
-    party_id: usize,
-    receiver: MultiReceiver<AstraSetupMsg>,
-    rng: ChaChaRng,
-    setup_data: Option<SetupData>,
-}
-
-impl Gate<R> for ArithmeticGate<R> {
-    type DimTy = ScalarDim;
-
-    fn is_interactive(&self) -> bool {
-        return false;
-    }
-
-    fn input_size(&self) -> usize {
-        match self {
-            ArithmeticGate::Base(base_gate) => base_gate.input_size(),
-            ArithmeticGate::Add => n,
-            ArithmeticGate::Mult => 2
-        }
-    }
-
-    fn as_base_gate(&self) -> Option<&BaseGate<bool>> {
-        match self {
-            BooleanGate::Base(base_gate) => Some(base_gate),
-            _ => None,
-        }
-    }
-
-    fn wrap_base_gate(base_gate: BaseGate<bool, Self::DimTy>) -> Self {
-        Self::Base(base_gate)
-    }
-}
-
-/*
 #[cfg(test)]
 mod tests {
-    use super::BooleanGate as BG;
+    use super::LoreleiGate as LG;
     use super::*;
     use crate::circuit::BaseCircuit;
     use crate::mul_triple::boolean::InsecureMTProvider;
@@ -192,14 +115,14 @@ mod tests {
     use bitvec::order::Lsb0;
 
     #[tokio::test]
-    async fn multi_and() {
-        let mut c = BaseCircuit::<bool, BG>::new();
-        let i0 = c.add_gate(BG::Base(BaseGate::Input(ScalarDim)));
-        let i1 = c.add_gate(BG::Base(BaseGate::Input(ScalarDim)));
-        let i2 = c.add_gate(BG::Base(BaseGate::Input(ScalarDim)));
-        let i3 = c.add_gate(BG::Base(BaseGate::Input(ScalarDim)));
-        let a = c.add_wired_gate(BG::And { n: 4 }, &[i0, i1, i2, i3]);
-        let _out = c.add_wired_gate(BG::Base(BaseGate::Output(ScalarDim)), &[a]);
+    async fn multi_mult() {
+        let mut c = BaseCircuit::<bool, LG>::new();
+        let i0 = c.add_gate(LG::Base(BaseGate::Input(ScalarDim)));
+        let i1 = c.add_gate(LG::Base(BaseGate::Input(ScalarDim)));
+        let i2 = c.add_gate(LG::Base(BaseGate::Input(ScalarDim)));
+        let i3 = c.add_gate(LG::Base(BaseGate::Input(ScalarDim)));
+        let a = c.add_wired_gate(LG::Mult { n: 4 }, &[i0, i1, i2, i3]);
+        let _out = c.add_wired_gate(LG::Base(BaseGate::Output(ScalarDim)), &[a]);
         let c = ExecutableCircuit::DynLayers(c.into());
 
         let (ch0, ch1) = seec_channel::in_memory::new_pair(16);
@@ -224,4 +147,3 @@ mod tests {
         assert_eq!(BitVec::<u8>::repeat(true, 1), res);
     }
 }
-*/
